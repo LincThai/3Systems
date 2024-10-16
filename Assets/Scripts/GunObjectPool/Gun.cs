82e21b8492e5ac88c69465d73000b6ff35b39ce3
LincThai <78945845+LincThai@users.noreply.github.com>
2024-10-16 18:28:46 +1100

    Start Object pool of bullets

--- Assets/Scripts/GunObjectPool/Gun.cs
+++ Assets/Scripts/GunObjectPool/Gun.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Gun : MonoBehaviour
 {
     // set variables
+    [SerializeField] private int ammoCapacity = 10;
+    [SerializeField] private int maxAmmoCount = 100;
+
     // spawn position
     public Transform bulletSpawn;
     // the bullet
-    public GameObject bulletPrefab;
+    public Bullet bulletPrefab;
+
+    private ObjectPool<Bullet> bulletPool;
+    public ObjectPool<Bullet> BulletPool => bulletPool;
 
     void Update()
     {
@@ -20,6 +28,16 @@ public class Gun : MonoBehaviour
 
     private void Shoot()
     {
-        Instantiate(bulletPrefab, bulletSpawn);
+        // make and setup object pool
+        bulletPool = new ObjectPool<Bullet>(() =>
+        {
+            // spawn bullet
+            Bullet bullet = Instantiate(bulletPrefab, bulletSpawn);
+            return bullet;
+        },
+        bullet => { bullet.gameObject.SetActive(true); },
+        bullet => { bullet.gameObject.SetActive(false); },
+        bullet => { Destroy(bullet.gameObject); },
+        false, ammoCapacity, maxAmmoCount);
     }
 }
